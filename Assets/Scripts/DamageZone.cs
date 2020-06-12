d917ba80b4cd7dce10da28e9584e9f13b5868cb5
Gilles Martin <gilles.pba@gmail.com>
2020-06-12 13:40:04 +0200

    World interaction - Projectile

---
+++ Assets/Scripts/DamageZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other){
+    	RubyController controller = other.GetComponent<RubyController>();
+
+	    if (controller != null)
+	    {
+	    	if (controller.health > 0) {
+	    		controller.ChangeHealth(-1);
+	    	}
+	        
+	    }
+    }
+}
